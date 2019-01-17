02f3cbb41ce1fea55d8661fb53fc1bc48b2bf7d1
Kelvin Tan <kelvin.vins@gmail.com>
2019-01-17 10:07:04 +0800

    Challenge 1. The makeAttributedString() method of ViewController transforms model data (strings) into different model data (an attributed string) – move that so it exists on the model itself, or wrap Project in a view model that does something similar.

--- Project/HackingWithSwift/Project.swift
+++ Project/HackingWithSwift/Project.swift
@@ -12,4 +12,16 @@ struct Project: Codable {
     var title: String
     var subtitle: String
     var topics: String
+
+    func makeAttributedString() -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+
+        let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+        let subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
+
+        titleString.append(subtitleString)
+
+        return titleString
+    }
 }
